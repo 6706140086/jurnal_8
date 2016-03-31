45e17b607e0cc106a2417d6411e9ca78a8227ba4
6706140086 <tisnadinata2@gmail.com>
2016-03-31 10:37:31 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -18,15 +18,20 @@ public class Nomor1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int a=0,b=0,hasil=1,c;
+         int a=0,b=0,hasil=1,c,hasil_jumlah=0,x=0;
+        int jumlah[] = new int[10];
         Scanner input = new Scanner (System.in);
         System.out.print ("Masukan Deret Fibonacci  : ");
         c = input.nextInt ();
         for(int i=1; i<=c; i++){           
             a=b;
             b=hasil;
-            System.out.print(hasil+" ");
+            jumlah[i] = hasil;
             hasil=a+b;
+            x++;
+        }
+        for(int i=x; i>=0; i--){           
+            System.out.print(jumlah[i]+" ");
         }
     } 
     
